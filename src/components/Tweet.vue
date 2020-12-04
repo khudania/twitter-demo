6bf04a4ff849bea7e3081416debbc7295213b850
Himanshu Khudania <hkhudania@gmail.com>
2020-12-04 08:41:28 +0530

    Small chnages

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -10,12 +10,13 @@
           <img src="profile.jpg" class="flex-none w-12 h-12 rounded-full border border-lighter"/>
         </div>
         <form v-on:submit.prevent = "addNewTweet" class="w-full px-4 relative">
-          <textarea v-model="tweet.content" placeholder="What's up?" class="mt-3 pb-3 w-full focus:outline-none"/>
+          <textarea v-model="tweet.content" placeholder="What's happening?" class="mt-3 pb-3 w-full focus:outline-none"/>
           <div class="flex items-center">
             <i class="text-lg text-blue mr-4 far fa-image"></i>
             <i class="text-lg text-blue mr-4 fas fa-film"></i>
             <i class="text-lg text-blue mr-4 far fa-chart-bar"></i>
             <i class="text-lg text-blue mr-4 far fa-smile"></i>
+            <i class="text-lg text-blue mr-4 fas fa-business-time"></i>
           </div>
           <button type="submit" class="h-10 px-4 text-white font-semibold bg-blue hover:bg-darkblue focus:outline-none rounded-full absolute bottom-0 right-0">
             Tweet
