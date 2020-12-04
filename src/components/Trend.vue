6679b3607f93b1f26fb49b8dc8be55821fbee7bb
Himanshu Khudania <hkhudania@gmail.com>
2020-12-04 10:07:52 +0530

    Added Trends area

---
+++ src/components/Trend.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="w-full rounded-lg bg-lightest">
+        <div class="flex items-center justify-between p-3">
+          <p class="text-lg font-bold">Trends for You</p>
+          <i class="fas fa-cog text-lg text-blue"></i>
+        </div>
+        <button v-for="trend in trending" :key="trend" class="w-full flex justify-between hover:bg-lighter p-3 border-t border-lighter">
+          <div>
+            <p class="text-xs text-left leading-tight text-dark"> {{ trend.top}} </p>
+            <p class="font-semibold text-sm text-left leading-tight"> {{ trend.title}} </p>
+            <p class="text-left text-sm leading-tight text-dark"> {{ trend.bottom}} </p>
+          </div>
+          <i class="fas fa-angle-down text-lg text-dark"></i>
+        </button>
+        <button class="p-3 w-full hover:bg-lighter text-left text-blue border-t border-lighter">
+          Show More
+        </button>
+      </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Trend',
+  components: {
+  },
+  data() {
+    return {
+      
+      trending: [
+        {top: 'COVID-19 LIVE', title: 'COVID-19 in India', bottom: ''},
+        {top: 'Agriculture · Trending', title: '#कल_किसानों_का_भारतबंद_रहेगा', bottom: '11.8K Tweets'},
+        {top: 'Trending in India', title: '#DiljitVsKangana', bottom: 'Trending with #DiljitDosanjh'},
+        {top: 'Music · Trending', title: '#कंगना_रानौत_शेरनी_है', bottom: '36.8k Tweets'},
+        {top: 'Trending in India', title: '#कल_भारत_बंद_रहेगा', bottom: 'Trending with #5दिसंबर_भारत_बंद, #कल_किसानों_का_भारतबंद_रहेगा'},
+        {top: 'United States politics · Trending', title: '#भारतीय_नौसेना_दिवस', bottom: '1028 Tweets'},
+      
+      ]
+    }
+  },
+}
+</script>
--- src/components/Trending.vue
+++ src/components/Trending.vue
@@ -2,18 +2,21 @@
     <!-- trending -->
     <div class="md:block hidden w-1/3 h-full border-l border-lighter py-2 px-6 overflow-y-scroll relative">
       <Search />
+      <Trend />
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Search from '@/components/Search'
+import Trend from '@/components/Trend'
 
 
 export default {
   name: 'Trending',
   components: {
    Search,
+   Trend
   
   },
 }
